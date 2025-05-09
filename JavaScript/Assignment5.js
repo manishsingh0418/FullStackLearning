1e83def05e7f9f2737894e6431a75c4658bf1709
Manish Kumar <manishkumar80819@gmail.com>
2025-05-09 20:26:37 +0530

    Assignment 5 complex functions

---
+++ JavaScript/Assignment5.js
@@ -0,0 +1,9 @@
+// Write a function that takes a user as an input and greets them with their name and age
+function greets(user){
+    console.log("Your name is "+user.name+" and your age is "+user.Age);
+}
+user ={
+    name:"Manish",
+    Age:21
+}
+greets(user);
