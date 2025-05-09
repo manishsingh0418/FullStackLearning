18a0162d582e259473b6ba59826f3bf2f500d127
Manish Kumar <manishkumar80819@gmail.com>
2025-05-09 20:39:56 +0530

    Assignment 6 updated assignment

--- JavaScript/Assignment6.js
+++ JavaScript/Assignment6.js
@@ -1,11 +1,18 @@
 // Write a function that takes a new object as input which has name , age  and gender and greets the user with their gender (Hi Mr/Mrs/Others harkirat, your age is 21)
+// Also tell the user if they are legal to vote or not
 function greet(user)
 {
     console.log("Hi "+user.name +" your age "+ user.age +" your gender is "+user.gender);
+    if(user.age>=18){
+        console.log("You are legal to vote");
+    }
+    else{
+        console.log("You are not legal to vote");
+    }
 }
 let user ={
     name:"Manish",
-    age:21,
+    age:17,
     gender:"Male"
 }
 greet(user);
